4d7aca513d67006ffa5f6259710e2336b853ef12
Julie Anzaroot <julieanzaroot@gmail.com>
2022-03-08 03:14:30 -0500

    final edit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function printResults(){
     for(let i = 1; i <= 100; i++){
-        if( i % 3 == 0){
+        if( i % 3 == 0 && i % 5 == 0){
+            console.log("CracklePop")
+        }
+        else if( i % 3 == 0){
             console.log("Crackle")
         }
         else if( i % 5 == 0){
